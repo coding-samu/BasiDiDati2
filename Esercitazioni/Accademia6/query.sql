665528f0d5611ad20c16c6d661154f8221dd288d
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-05-24 08:01:38 +0200

    completed Accademia6

--- Esercitazioni/Accademia6/query.sql
+++ Esercitazioni/Accademia6/query.sql
@@ -14,19 +14,19 @@ SELECT avg(oreDurata) as media, min(oreDurata) as minimo, max(oreDurata) as mass
 select per.id as id_persona, per.nome, per.cognome, avg(ap.oreDurata) as media, min(ap.oreDurata) as minimo, max(ap.oreDurata) as massimo FROM persona as per, progetto as pro, attivitaprogetto as ap WHERE pro.id = ap.progetto AND pro.nome = 'Pegasus' AND ap.persona = per.id group by per.id;
 
 -- Esercizio 6
-
+select per.id as id_persona, per.nome, per.cognome, sum(anp.oreDurata) as ore_didattica FROM persona as per, attivitanonprogettuale as anp WHERE per.id = anp.persona and anp.tipo = 'Didattica' GROUP BY per.id;
 
 -- Esercizio 7
-
+select avg(stipendio) as media, min(stipendio) as minimo, max(stipendio) as massimo FROM persona WHERE posizione = 'Ricercatore'; 
 
 -- Esercizio 8
-
+select posizione, avg(stipendio) as media, min(stipendio) as minimo, max(stipendio) as massimo FROM persona GROUP BY posizione;
 
 -- Esercizio 9
-
+select pr.id as id_progetto, pr.nome as progetto, sum(ap.oreDurata) FROM progetto as pr, attivitaprogetto as ap, persona as per WHERE pr.id = ap.progetto AND ap.persona = per.id AND per.nome = 'Ginevra' and per.cognome = 'Riva' GROUP BY pr.id;
 
 -- Esercizio 10
-
+select id_progetto, progetto from (select pr.id as id_progetto, pr.nome as progetto, count(ap.persona) as somma FROM progetto as pr, attivitaprogetto as ap WHERE ap.progetto = pr.id group by pr.id) as ta WHERE somma > 2;
 
 -- Esercizio 11
-
+select id_persona, nome, cognome FROM (select per.id as id_persona, per.nome, per.cognome, sum(ap.progetto) as somma FROM persona as per, attivitaprogetto as ap WHERE per.posizione = 'Professore Associato' and ap.persona = per.id group by per.id) as ta WHERE somma > 1;
