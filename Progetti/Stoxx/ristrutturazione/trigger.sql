9629ef7c311d186d1a5092b227e2d70e8ddfe5ab
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-06-25 17:16:06 +0200

    updated Stoxx

--- Progetti/Stoxx/ristrutturazione/trigger.sql
+++ Progetti/Stoxx/ristrutturazione/trigger.sql
@@ -25,9 +25,6 @@
             WHERE t.sf = new.id
         )
 
-[V.Emittente_Stato_se_e_solo_se_titolo_di_stato]
-ALL sf,e,te,tt,t em_sf(e,sf) and t_isa_sf(t,sf) and tipoEmittente(e,te) and te = 'Stato' <-> Titolo(t) and tipoTitolo(t,tt) and tt = 'DiStato'
-
 4. Trigger V.Emittente_Stato_se_e_solo_se_titolo_di_stato
     quando deve essere effettuato: dopo insert(new) o update(new) in StrumentoFinanziario
     controllo da effettuare:
@@ -37,21 +34,56 @@ ALL sf,e,te,tt,t em_sf(e,sf) and t_isa_sf(t,sf) and tipoEmittente(e,te) and te =
             WHERE t.sf = new.id and e.nome = new.emittente and ((e.tipo = 'Stato' and t.tipo <> 'DiStato') or (e.tipo <> 'Stato' and t.tipo = 'Stato'))
         )
 
-[V.Istante_investimento_in_periodo_gestione]
-ALL inv,ges,i,is inv_ges(inv,ges) and inizio(ges,i) and istante(inv,is) -> 
-    (isTerminata(g,True) and fine(ges,f) and i <= is and is <= f)
-    or
-    (isTerminata(g,False) and i <= is)
+5. Trigger V.Istante_investimento_in_periodo_gestione
+    quando deve essere effettuato: dopo insert(new) o update(new) in Investimento
+    controllo da effettuare:
+        isError := not exists(
+            select *
+            from Gestione g
+            where new.gestione = g.id and new.istante >= g.inizio and (g.fine is null or g.fine >= new.istante)
+        )
 
-[V.Gestione.Overlapping]
-ALL c,g',g'',i',i'' cli_ges(c,g') and cli_ges(c,g'') and inizio(g',i') and inizio(g'',i'')
-    -> NOT EXISTS t DataOra(t) and (i' <= t and i'' <= t) and (ALL f fine(g',f) -> t <= f) and (ALL f fine(g'',f) -> t <= f)
+6. Trigger V.Gestione.Overlapping
+    quando deve essere effettuato: dopo insert(new) o update(new) in Gestione
+    controllo da effettuare:
+        isError := exists (
+            select *
+            from Gestione g
+            where g.id != new.id and new.cliente = g.cliente and
+                    ((g.fine is not null and new.fine is not null and (g.inizio,g.fine) overlaps (new.inizio,new.fine))
+                    or (g.fine is not null and new.fine is null and (g.inizio,g.fine) overlaps (new.inizio,now()::timestamp))
+                    or (g.fine is null and new.fine is not null and (g.inizio,now()::timestamp) overlaps(new.inizio,new,fine))
+                    or (g.fine is null and new.fine is null)
+                    )
+        )
 
-[V.Istante_investimento_maggiore_di_rilevazione]
-ALL inv,sf,i inv_sf(inv,sf) and istante(inv,i) -> EXISTS r,ri ril_sf(r,sf) and istante(r,ri) and ri <= i
+7. Trigger V.Istante_investimento_maggiore_di_rilevazione
+    quando deve essere effettuato: dopo insert(new) o update(new) in Investimento
+    controllo da effettuare:
+        isError := not EXISTS (
+            select *
+            from Rilevazione r
+            where r.istante <= new.istante and r.sf = new.sf
+        )
 
-[V.Istante_disinvestimento_maggiore_di_istante_investimento]
-ALL dis,inv,i,id dis_inv(dis,inv) and istante(inv,i) and istante(dis,id) -> id >= i
+8. Trigger V.Istante_disinvestimento_maggiore_di_istante_investimento
+    quando deve essere effettuato: dopo insert(new) o update(new) in Disinvestimento
+    controllo da effettuare:
+        isError := EXISTS (
+            select *
+            from Investimento i
+            where i.istante > new.istante and new.investimento = i.id
+        )
 
-[V.Somma_quantita_disinvestimento_LEQ_quantita_investimento]
-ALL inv,qi Investimento(inv) and quantita(inv,qi) and Q = {(q,d) | dis_inv(d,i) and quantita(d,q)} and tot = (sum_(q,d) in Q q) -> tot <= qi
+9. Trigger V.Somma_quantita_disinvestimento_LEQ_quantita_investimento
+    quando deve essere effettuato: dopo insert(new) o update(new) in Disinvestimento
+    controllo da effettuare:
+        isError := EXISTS (
+            select *
+            from tot as (
+                select sum(d.quantita)
+                from Disinvestimento d
+                where d.investimento = new.investimento
+            ), Investimento i
+            where new.investimento = i.id and tot > i.quantita
+        )
