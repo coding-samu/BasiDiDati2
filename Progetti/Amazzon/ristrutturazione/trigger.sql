17f2444df5e98083fb667341f68778fa15c23915
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-06-28 11:47:43 +0200

    updated Amazzon

--- Progetti/Amazzon/ristrutturazione/trigger.sql
+++ Progetti/Amazzon/ristrutturazione/trigger.sql
@@ -1,161 +1,144 @@
-/*
-[V.Offerta.No_overlapping_offerte_stesso_articolo_per_stessa_azienda]
-ALL n,a,o1,o2,i1,i2 neg_of(n,o1) and neg_of(n,o2) and art_of(a,o1) and art_of(a,o2) and inizio(o1,i1) and inizio(o2,i2)
-    -> not exists t DataOra(t) and (i1 <= t and i2 <= t) and ALL (f1 fine(o1,f1) -> t <= f1) and (ALL f2 fine(o2,f2) -> t <= f2)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Offerta.No_overlapping_offerte_stesso_articolo_per_stessa_azienda
+    quando deve essere effettuato: dopo insert(new) o update(new) in Offerta
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Offerta o
+            where o.id != new.id and o.articolo = new.articolo and o.negozio = new.negozio and ((o.inizio,coalesce(o.fine,'infinity'::timestamp)) overlaps (new.inizio,coalesce(new.fine,'infinity'::timestamp)))
+        );
 
-/*
-[V.Riduzione.Riduzioni_consistenti]
-ALL r,n,o,i spedizione(n,o) and rid_spe(r,n,o) and inizio(r,i) and i != 2 -> EXISTS r1,f r1 != r and rid_spe(r1,n,o) and f = i-1
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Riduzione.Riduzioni_consistenti
+    quando deve essere effettuato: dopo insert(new) o update(new) o remove(new)
     controllo da effettuare:
-        isError := 
+        isError := exists ( -- i1 < i
+            select *
+            from Riduzione r
+            where r.spedizione = new.spedizione and r.id <> new.id and r.inizio < new.inizio
+        ) and not exists ( -- f1 = i-1
+            select *
+            from Riduzione r
+            where r.spedizione = new.spedizione and r.id <> new.id and r.fine = new.inizio-1
+        )
 
-/*
-[V.Riduzione.No_overlapping_stessa_spedizione]
-ALL r1,r2,i1,i2,n,o spedizione(n,o) and rid_spe(r1,n,o) and rid_spe(r2,n,o) and inizio(r1,i1) and inizio(r2,i2)
-    -> not exists t DataOra(t) and (i1 <= t and i2 <= t) and (ALL f1 fine(r1,f1) -> t <= f1) and (ALL f2 fine(r2,f2) -> t <= f2)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Riduzione.No_overlapping_stessa_spedizione
+    quando deve essere effettuato: dopo insert(new) o update(new) in Riduzione
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Riduzione r
+            where r.spedizione = new.spedizione and ((new.inizio,coalesce(new.fine,'infinity'::integer)) overlaps (r.inizio,coalesce(r.fine,'infinity'::integer)))
+        );
 
-/*
-[V.Riduzione.piu_grande_senza_fine]
-ALL r,n,o rid_spe(r,n,o) -> EXISTS r1 rid_spe(r1,n,o) and NOT EXISTS f fine(r1,f)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Riduzione.piu_grande_senza_fine
+    quando deve essere effettuato: dopo insert(new), update(new) o delete(new) in Riduzione
     controllo da effettuare:
-        isError := 
+        isError := not exists (
+            with massimo as (
+                select max(r.inizio) as ma
+                from Riduzione r
+                where r.spedizione = new.spedizione
+            )
+            select *
+            from Riduzione r, massimo mas
+            where r.inizio = mas.ma and r.fine is null and r.spedizione = new.spedizione
+        );
 
-/*
-[V.Acquisto.buoni_regalo_posseduti_da_utente]
-ALL a,b,u acq_ut(a,u) and acq_br(a,b) -> possiede(u,b)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.buoni_regalo_posseduti_da_utente
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from BuonoRegalo br
+            where br.acquisto = new.id and br.possiede <> new.utente
+        );
 
-/*
-[V.Acquisto.totale_buoni_non_supera_prezzo]
-ALL a,psb,tb prezzoSenzaBuoni_{Acquisto}(a,psb) and valoreBuoni{Acquisto}(a,tb) -> psb-tb >= 0
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.totale_buoni_non_supera_prezzo
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := valoreBuoni(new.id) < prezzoSenzaBuoni(new.id)
 
-/*
-[V.Acquisto.indirizzo_nazione_tale_che_esiste_spedizione_nazione]
-ALL a,ca,na acq_cit(a,ca) and cit_naz(ca,na) -> EXISTS o acq_of(a,o) and spedizione(na,o)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.indirizzo_nazione_tale_che_esiste_spedizione_nazione
+    quando deve essere effettuato: dopo insert(new) o update(new) in acq_of
     controllo da effettuare:
-        isError := 
+        isError := not exists (
+            select *
+            from Citta c, Acquisto acq, spedizione s
+            where acq.citta = c.id and new.acquisto = acq.id and new.offerta = s.offerta and s.nazione = c.nazione
+        );
 
-/*
-[V.Acquisto.acquisto_in_periodo_offerta]
-ALL a,o,is,i acq_of(a,o) and istante(a,is) and inizio(a,i) -> (ALL f fine(o,f) -> is <= f) and i <= is
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.acquisto_in_periodo_offerta
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Offerta o
+            where new.offerta = o.id and not ((new.istante,new.istante) overlaps (o.inizio,coalesce(o.fine,'infinity'::timestamp)))
+        );
 
-/*
-[V.Acquisto.buono_utilizzato_in_periodo_validità]
-ALL a,b,is,i,f acq_br(a,b) and inizio(b,i) and fine_{BuonoRegalo}(b,f) -> i <= is <= f
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.buono_utilizzato_in_periodo_validità
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from BuonoRegalo br
+            where br.acquisto = new.id and (new.istante < br.inizio or new.istante > fine(br.id))
+        );
 
-/*
-[V.Acquisto.istante_acquisto_maggiore_registrazione_utente]
-ALL a,u,ia,ir acq_ut(a,u) and reg(u,ir) and istante(a,ia) -> ia > ir
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.istante_acquisto_maggiore_registrazione_utente
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Utente u
+            where new.utente = u.nickname and u.reg > new.istante
+        );
 
-/*
-[V.BuonoRegalo.istante_inizio_maggiore_registrazione_utente_che_possiede]
-ALL u,b,ir,i possiede(u,b) and reg(u,ir) and inizio(b,i) -> ir <= i
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.BuonoRegalo.istante_inizio_maggiore_registrazione_utente_che_possiede
+    quando deve essere effettuato: dopo insert(new) o update(new) in BuonoRegalo
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Utente u
+            where new.possiede = u.nickname and u.reg > new.inizio
+        );
 
-/*
-[V.BuonoRegalo.istante_inizio_maggiore_registrazione_utente_che_acquista]
-ALL u,b,ir,i acquista(u,b) and reg(u,ir) and inizio(b,i) -> ir <= i
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.BuonoRegalo.istante_inizio_maggiore_registrazione_utente_che_acquista
+    quando deve essere effettuato: dopo insert(new) o update(new) in BuonoRegalo
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from Utente u
+            where new.acquista = u.nickname and u.reg > new.inizio
+        );
 
-/*
-[V.Acquisto.cc_di_utente]
-ALL a,cc,u acq_cc(a,cc) and acq_ut(a,u) -> cc_ut(cc,u)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.cc_di_utente
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from CartaDiCredito c
+            where new.carta = c.numero and c.utente <> new.utente
+        );
 
-/*
-[V.Acquisto.cc_solo_se_prezzo_GZ]
-ALL a,p prezzo_{Acquisto}(a,p) (EXISTS cc acq_cc(a,cc) <-> p > 0)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.cc_solo_se_prezzo_GZ
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := (prezzo(new.id) > 0 and new.carta is null) or (prezzo(new.id) = 0 and new.carta is not null);
 
-/*
-[V.Acquisto.cc_non_scaduta]
-ALL a, cc, ia, is acq_cc(a,cc) and istante(a,ia) and scadenza(cc,is) -> ia <= is
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Acquisto.cc_non_scaduta
+    quando deve essere effettuato: dopo insert(new) o update(new) in Acquisto
     controllo da effettuare:
-        isError := 
+        isError := exists (
+            select *
+            from CartaDiCredito c
+            where c.numero = new.carta and c.scadenza < new.istante
+        );
 
-/*
-[V.Amicizia.no_due_relazioni]
-ALL u1,u2 amiciziaPending(u1,u2) -> not amiciziaPending(u2,u1)
-*/
-TODO
--- Trigger
-    quando deve essere effettuato: dopo
+-- Trigger V.Amicizia.no_due_relazioni
+    quando deve essere effettuato: dopo insert(new) o update(new) in amiciziaPending
     controllo da effettuare:
-        isError :=
+        isError := exists (
+            select *
+            from amiciziaPending ap
+            where ap.riceve = new.invia and ap.invia = new.riceve
+        );
